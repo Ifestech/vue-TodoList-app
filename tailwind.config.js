54cf73c6f1d0acd1bba8d8ca5516ef79e8b33509
Egbonimale Raymond <egbonimaleraymornd2@gmail.com>
2025-08-21 11:29:12 -0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class', // 👈 this is required
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
